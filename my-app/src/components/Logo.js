699a38ddeb81e8ee8282c2623d48372f78b7b52b
Julius Regalado <jreg03@outlook.com>
2022-04-02 14:29:42 -0400

    react integration - just scaffolding and mainitenance page

---
+++ my-app/src/components/Logo.js
@@ -0,0 +1,10 @@
+import logo from '../images/logo.svg';
+
+function Logo() {
+  return (
+      <div className="react-logo-contianer">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+  );
+}
+export default Logo;
---
+++ my-app/src/components/Maintenance.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Logo from './Logo';
+
+function Maintenance() {
+    return (
+        <div className="maintenance-wrapper">
+            <div className="inner-content">
+                <h2>Good Stuff are Coming Soon</h2>
+                <h1>Stay Positive</h1>
+                <Logo />
+            </div>
+        </div>
+    );
+}
+export default Maintenance;
